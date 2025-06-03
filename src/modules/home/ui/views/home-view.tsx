8cff7c114555e019b51670e8b3df7594ba88dec1
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2025-06-03 22:39:37 +0400

    still error on tRPC types

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -5,7 +5,7 @@ import { useQuery } from "@tanstack/react-query";
 
 export const HomeView = () => {
   const trpc = useTRPC();
-  const { data } = useQuery<{ greeting: string }>(
+  const { data } = useQuery(
     trpc.hello.queryOptions({
       text: "Erika",
     })
