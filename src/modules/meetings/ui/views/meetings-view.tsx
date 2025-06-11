92927bd17b7c15a2e76129b4d2edba4a29e6dcdf
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2025-06-11 20:35:38 +0400

    refactor(meetings): remove unused useQuery import and clean up formatting

--- src/modules/meetings/ui/views/meetings-view.tsx
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -3,18 +3,14 @@
 import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
 import { useTRPC } from "@/trpc/client";
-import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 
 export const MeetingsView = () => {
   const trpc = useTRPC();
-  const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}))
+  const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
 
-  return (
-    <div>
-      {JSON.stringify(data)}
-    </div>
-  )
-}
+  return <div>{JSON.stringify(data)}</div>;
+};
 
 export const MeetingsViewLoading = () => {
   return (
